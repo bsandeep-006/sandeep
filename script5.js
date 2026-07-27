fa184a68023b5aa17978b1ab0aa2d0f3b2a5ec9b
bsandeep-006 <bsandeepb6@gmail.com>
2026-07-27 14:05:04 +0530

    Update script5.js

---
+++ script5.js
@@ -0,0 +1,19 @@
+// Function Declaration
+function greet() {
+    console.log("Hello from a function declaration!");
+}
+
+// Function Expression
+const sayGoodbye = function () {
+    console.log("Goodbye from a function expression!");
+};
+
+// Arrow Function
+const sayThanks = () => {
+    console.log("Thanks from an arrow function!");
+};
+
+// Function Calls
+greet();         // Calls the function declaration
+sayGoodbye();    // Calls the function expression
+sayThanks();     // Calls the arrow function
